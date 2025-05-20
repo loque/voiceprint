3d68cb3133a7e61ec93a69fdf801dc75f2aede2a
Lucas Soler <hola@lucassoler.com.ar>
2025-05-20 16:49:01 -0300

    Add voice retrieval endpoint and update .gitignore for voices directory

--- voiceprint/http_server/server.py
+++ voiceprint/http_server/server.py
@@ -3,6 +3,20 @@ from flask import Blueprint, request, jsonify, current_app
 
 main = Blueprint('main', __name__)
 
+@main.route('/voices', methods=['GET'])
+def get_voices():
+    voices_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../voices'))
+    voices = {}
+    if not os.path.exists(voices_dir):
+        return jsonify({}), 200
+    for entry in os.listdir(voices_dir):
+        entry_path = os.path.join(voices_dir, entry)
+        if os.path.isdir(entry_path):
+            wav_files = [f for f in os.listdir(entry_path)
+                         if os.path.isfile(os.path.join(entry_path, f)) and f.lower().endswith('.wav')]
+            voices[entry] = wav_files
+    return jsonify(voices), 200
+
 @main.route('/identify', methods=['POST'])
 def identify_endpoint():
     if 'audio_file' not in request.files:
